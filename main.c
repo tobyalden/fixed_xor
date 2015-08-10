38a5ccbe88963ef74c0518f6e59db6ee0198b9ae
Toby Alden <toby.alden@gmail.com>
2015-08-10 15:23:43 -0700

    Refactor.

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <gmp.h>
 
-void fixed_xor(char *hex, char *mask);
+char * fixed_xor(char *hex, char *mask);
+void printstr(char *str);
 
 int main(int argc, char *argv[])
 {
   printf("HEX INPUT: %s\n", argv[1]);
   printf("HEX MASK: %s\n", argv[2]);
-  fixed_xor(argv[1], argv[2]);
+  printstr(fixed_xor(argv[1], argv[2]));
 }
 
-void fixed_xor(char *hex, char *mask)
+char * fixed_xor(char *hex, char *mask)
 {
   mpz_t decimal;
   mpz_t decimal_mask;
@@ -24,20 +25,14 @@ void fixed_xor(char *hex, char *mask)
   char *xored_hex = mpz_get_str(NULL, 16, xored_decimal);
 
   printf("XORED HEX: ");
-  for(int i = 0; xored_hex[i] != '\0'; i++)
+  return xored_hex;
+}
+
+void printstr(char *str)
+{
+  for(int i = 0; str[i] != '\0'; i++)
   {
-    printf("%c", xored_hex[i]);
+    printf("%c", str[i]);
   }
   printf("\n");
 }
-
-// — Function: char * mpz_get_str (char *str, int base, const mpz_t op)
-// Convert op to a string of digits in base base. The base argument may vary from 2 to 62 or from −2 to −36.
-//
-// For base in the range 2..36, digits and lower-case letters are used; for −2..−36, digits and upper-case letters are used; for 37..62, digits, upper-case letters, and lower-case letters (in that significance order) are used.
-//
-// If str is NULL, the result string is allocated using the current allocation function (see Custom Allocation). The block will be strlen(str)+1 bytes, that being exactly enough for the string and null-terminator.
-//
-// If str is not NULL, it should point to a block of storage large enough for the result, that being mpz_sizeinbase (op, base) + 2. The two extra bytes are for a possible minus sign, and the null-terminator.
-//
-// A pointer to the result string is returned, being either the allocated block, or the given str.
